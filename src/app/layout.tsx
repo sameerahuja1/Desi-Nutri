ffaaa68c250f0fd6bf099b4fe162f31cd9cccaa9
Sameer Ahuja <sameerahuja12112@gmail.com>
2025-09-05 19:50:56 +0000

    make it compitble and nicely working for. phone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
             </Sidebar>
             <SidebarInset>
                  <Header />
-                <main className="flex-1 p-4 sm:p-6">{children}</main>
+                <main className="flex-1 p-2 sm:p-6">{children}</main>
             </SidebarInset>
         </SidebarProvider>
         <Toaster />
