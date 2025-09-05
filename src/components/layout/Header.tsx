df3e6435d9eae77b0a1defaa79f2a06b9d1f8e06
Sameer Ahuja <sameerahuja12112@gmail.com>
2025-09-05 20:04:51 +0000

    make it a 3 line menubar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="flex items-center">
-            <SidebarTrigger className="h-8 w-8" />
+            <SidebarTrigger className="h-10 w-10" />
              <Link href="/" className="ml-2 mr-6 flex items-center space-x-2">
                 <UtensilsCrossed className="h-6 w-6 text-primary" />
                 <span className="font-bold inline-block">DesiNutri</span>
